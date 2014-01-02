d98a7222b3d88eb2150e7d451850e9bedaf379ee
Jihun Jeong <jeongj2014@tciscommunity.com>
2014-01-02 13:27:52 +0900

    hey

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,9 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "500x500>" }
 
 	validates :image, presence: true
-	validates :word, presence: true
-	validates :definition, presence: true
+	validates :description, presence: true
+
+	def definition 
+		description 
+	end
 end
